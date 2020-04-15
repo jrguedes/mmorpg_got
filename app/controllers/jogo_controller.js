c933e9b2e765233381d2bcee96fac9a33e59ad30
jrguedes <jrguedes.ja@gmail.com>
2020-04-15 13:46:43 -0300

    Sessao de usuario está sendo controlada

--- app/controllers/jogo_controller.js
+++ app/controllers/jogo_controller.js
@@ -1,3 +1,13 @@
-module.exports.renderJogo = function(application, req, res){
-    application.app.controllers.jogo_controller.renderJogo(application, req, res);
+module.exports.renderJogo = function (application, req, res) {
+    if (req.session.autorizado) {
+        res.render('jogo');
+    } else {
+        res.send('Usuário não autenticado!');
+    }
+}
+
+module.exports.sair = function (application, req, res) {    
+    req.session.destroy(function (err) {
+        res.render('index', { validacaoErros: [] });
+    });
 }
--- app/routes/jogo_route.js
+++ app/routes/jogo_route.js
@@ -1,5 +1,9 @@
-module.exports = function(application){
-    application.get('/jogo', function(req, res){
-        res.render('jogo');
+module.exports = function (application) {
+    application.get('/jogo', function (req, res) {
+        application.app.controllers.jogo_controller.renderJogo(application, req, res);
+    });
+
+    application.get('/sair', function (req, res) {
+        application.app.controllers.jogo_controller.sair(application, req, res);
     });
 }
